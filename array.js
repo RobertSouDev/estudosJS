edbdfd4a52672279b04507551f0327552e9d78ac
roger araujo <roger0784@digitalcollegesul.local>
2023-12-01 22:07:11 -0300

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+ // Array 
+ const animals  = [
+    'lion',
+    'Monkey',
+    'Cat'
+ ]
+
+ // acessar valores dentro do Array
+
+console.log(animals[0]) // posição do array para pega ver cada elemento a lista comeca paparti do zero
+
+console.log(animals[3]) // se nao tiver vai retorna undefined
+
+console.log(animals.length) // para ver o tamanho da lista
+
+// lembrando para nao mistura 
