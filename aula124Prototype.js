fe122f2416c64e8ba110ac3403df879d63b1b602
RobertSouDev <rogerhallks1@gmail.com>
2023-12-05 00:26:16 -0300

    Prototype

---
+++ aula124Prototype.js
@@ -0,0 +1,14 @@
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__
+    
+*/
+"mayk".__proto__
+23.0.__proto__
+
+//ex 
+let teste = "mayk"
+console.log("Mayk tem caracteres  + teste.length")
